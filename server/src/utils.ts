e320588217f4bb320b0b74a40e70f19ba670d833
porridgewithraisins <santhanambr2002@gmail.com>
2022-04-06 23:50:27 +0530

    remove unused

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -1,17 +1,3 @@
-class HttpError extends Error {
-    constructor(message: string, public status: number) {
-        super(message);
-        this.name = "HttpError";
-    }
-}
-
-export class BadRequestError extends HttpError {
-    constructor(message: string) {
-        super(message, 403);
-        this.name = "BadRequestError";
-    }
-}
-
 export const wrangleCheckbox = ({ is_day_scholar, ...rest }: any) => ({
     ...rest,
     is_day_scholar: is_day_scholar === "on" ? 1 : 0,
